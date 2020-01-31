c5297381d94d4765316445ce3ad04343bf62b26e
chrisxcoto <53847773+chrisxcoto@users.noreply.github.com>
2020-01-31 07:41:39 -0500

    added crystals and gave them values

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+    var win = ""
+    var losses = ""
+    var currentScore = ""
+    var goal = ""
+    
+    var crystalOne = 0;
+    var crystalTwo = 0;
+    var crystalThree = 0;
+    var crystalFour = 0;
+    // var randomNum = 0;
+    
+    function getNum(min, max) {
+        return Math.floor((Math.random() * max) + min);
+    }    
+
+function makeCrystals(){    
+    $("#crystals").empty();
+
+
+    for (let i=1; i <= 4; i++){
+
+        var $crystal = $("<img>")
+            .attr("src", "https://pbs.twimg.com/profile_images/531994916181012480/EBap51cO_400x400.png")
+
+            .attr("data-value", getNum(1,10))
+            .addClass("crystalOption")
+
+            $("#crystals").append($crystal);
+
+
+    }
+    
+}
+
+function newGame(){
+    goal = getNum(30, 120);
+    $("#Total").html(goal);
+    currentScore = 0
+    $("#user").html(currentScore);
+
+    makeCrystals()
+}
+
+
+$(document).ready(newGame)
+
+
+
+    
+    /* crystalThree = Math.floor((Math.random() * 120) +1 );
+    console.log(crystalThree);
+    crystalFour = Math.floor((Math.random() * 120) +1 );
+    console.log(crystalFour); */
+
