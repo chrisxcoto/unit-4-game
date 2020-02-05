585dd8b95fe933bd163ba0fc349b0ce5bfe2562a
chrisxcoto <53847773+chrisxcoto@users.noreply.github.com>
2020-02-05 18:41:39 -0500

    added class to score var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,44 +11,56 @@
     
     function getNum(min, max) {
         return Math.floor((Math.random() * max) + min);
-    }    
+
+    };
 
 function makeCrystals(){    
     $("#crystals").empty();
 
-
     for (let i=1; i <= 4; i++){
 
-        var $crystal = $("<img>")
+        const crystal = $("<img>")
             .attr("src", "https://pbs.twimg.com/profile_images/531994916181012480/EBap51cO_400x400.png")
 
             .attr("data-value", getNum(1,10))
             .addClass("crystalOption")
 
-            $("#crystals").append($crystal);
+            $("#crystals").append(crystal);
 
-
-    }
-    
+            console.log(crystal);
 }
+    }
 
 function newGame(){
     goal = getNum(30, 120);
     $("#Total").html(goal);
+
     currentScore = 0
     $("#user").html(currentScore);
 
     makeCrystals()
 }
 
+function giveValue(){
+
+    $("#crystals").on("click", function(){
+        currentScore += crystal; 
+        $("#currentPoints").html(currentNumber);
+        if (currentNumber === randomNumber){
+            win();
+        } else if (currentNumber > randomNumber){
+            lose();
+        };
+    });
+    
+
 
 $(document).ready(newGame)
 
 
-
-    
     /* crystalThree = Math.floor((Math.random() * 120) +1 );
     console.log(crystalThree);
     crystalFour = Math.floor((Math.random() * 120) +1 );
     console.log(crystalFour); */
+
 
---
+++ assets/javascript/game2.js
@@ -0,0 +1,84 @@
+var win = 0;
+var losses = 0;
+var currentScore = 0;
+var goal = 0;
+
+var randomNumber = getNum(1, 10);
+
+var crystalOne = 0;
+var crystalTwo = 0;
+var crystalThree = 0;
+var crystalFour = 0;
+// var randomNum = 0;
+
+function getNum(min, max) {
+    return Math.floor((Math.random() * max) + min);
+
+};
+
+function makeCrystals(){    
+$("#crystals").empty();
+
+for (let i=1; i <= 4; i++){
+
+    var $crystal = $("<img>")
+        .attr("src", "https://wiki.guildwars2.com/images/8/88/Gem_%28highres%29.png")
+
+        .attr("data-value", getNum(1,10))
+        .addClass("crystalOption")
+
+        $("#crystals").append($crystal);
+
+            console.log(($crystal).attr("data-value"))
+            console.log($crystal);
+}};
+
+
+   $(document).on("click",'.crystalOption', function(event){
+    // currentScore += this.data-value;
+    currentScore += parseInt($(this).attr('data-value'));
+    $("#user").html(currentScore); 
+
+    //create an if else statement for win/lose situations
+    //create a function to reset the game after win/lose
+
+    
+});
+
+
+function win()
+
+if (currentScore == goal){
+win++
+$("#wins").html("Wins:" + wins);
+intialize();
+
+}
+
+console.log(win);
+
+
+
+function newGame(){
+goal = getNum(30, 120);
+$("#Total").html(goal);
+
+currentScore = 0
+$("#user").html(currentScore);
+
+makeCrystals()
+};
+
+    /*if (currentNumber === randomNumber){
+        win();
+    } else if (currentNumber > randomNumber){
+        lose();
+    }; */
+
+$(document).ready(newGame);
+
+
+/* crystalThree = Math.floor((Math.random() * 120) +1 );
+console.log(crystalThree);
+crystalFour = Math.floor((Math.random() * 120) +1 );
+console.log(crystalFour); */
